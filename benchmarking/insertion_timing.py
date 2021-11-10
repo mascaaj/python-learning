26becdfae3ab934b0756d60a7272891a76b08583
aloke <boo@batmanwsl>
2021-11-10 15:28:07 -0600

    minor edit, removing redundant files

--- benchmarking/insertion_timing.py
+++ benchmarking/insertion_timing.py
@@ -1,19 +1,20 @@
 import sys
 import os
 import time
-sys.path.append("../scripts")
+sys.path.append("../scripts/linked_lists")
 
 from linked_lists import LinkedList
 from double_linked_lists import DoubleLinkedList
 
 if __name__=="__main__":
 
+    max_size = 500000
     array=[]
     lltest = LinkedList()
     dlltest = DoubleLinkedList()
 
     time_start = time.time()
-    for i in range(50000):
+    for i in range(max_size):
         lltest.insert_start(i)
     time_stop = time.time()
     lltest.node_count()
@@ -21,7 +22,7 @@ if __name__=="__main__":
     print("Delta time for linked list insertion", delta_time)
 
     time_start = time.time()
-    for i in range(50000):
+    for i in range(max_size):
         dlltest.insert(i)
     time_stop = time.time()
     dlltest.node_count()
@@ -29,7 +30,7 @@ if __name__=="__main__":
     print("Delta time for double linked list insertion", delta_time)
 
     time_start = time.time()
-    for i in range(50000):
+    for i in range(max_size):
         array.insert(0 , i)
     time_stop = time.time()
     print("current array size : ", len(array))
